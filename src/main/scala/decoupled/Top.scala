88082baa858c71e660b2a6d44545d6a5936721de
Hadir Khan <hadirkhan10@gmail.com>
2020-08-18 14:59:34 +0500

    Modified the Top module to pass values to the Module A and test if they are being received by Module B

--- src/main/scala/decoupled/Top.scala
+++ src/main/scala/decoupled/Top.scala
@@ -4,13 +4,20 @@ import chisel3.util._
 
 class Top extends Module {
   val io = IO(new Bundle {
-    val testOut = Output(UInt(1.W))
+    val addr_in = Input(UInt(32.W))
+    val data_in = Input(UInt(32.W))
+    val reg1 = Output(UInt(32.W))
+    val reg2 = Output(UInt(32.W))
   })
 
   val moduleA = Module(new ModuleA)
   val moduleB = Module(new ModuleB)
 
-  moduleA.io <> moduleB.io
+  moduleA.io.bundle <> moduleB.io.bundle
+  moduleA.io.top_addr := io.addr_in
+  moduleA.io.top_data := io.data_in
 
-  io.testOut := 1.U
+
+  io.reg1 := moduleB.io.reg1_data
+  io.reg2 := moduleB.io.reg2_data
 }
--- src/test/scala/decoupled/TopTester.scala
+++ src/test/scala/decoupled/TopTester.scala
@@ -2,5 +2,8 @@ package decoupled
 import chisel3.iotesters._
 
 class TopTester(c: Top) extends PeekPokeTester(c) {
-  step(10)
+  poke(c.io.addr_in, 0)
+  poke(c.io.data_in, 20)
+  step(3)
+  expect(c.io.reg1, 20)
 }
