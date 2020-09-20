ea534f200b1773b59dcd001b49e63e07d6280165
Hadir Khan <hadirkhan10@gmail.com>
2020-09-20 22:14:37 +0500

    initialised tilelink bundle

--- src/main/scala/decoupled/ModuleA.scala
+++ src/main/scala/decoupled/ModuleA.scala
@@ -1,6 +1,7 @@
 package decoupled
 import chisel3._
 import chisel3.util._
+import tilelink.TL_H2D
 
 // Decoupled provides port interface of three signals
 // -> ready (Input)
@@ -23,6 +24,7 @@ class ModuleA extends Module {
   val io = IO(new Bundle {
     val top_addr = Input(UInt(32.W))
     val top_data = Input(UInt(32.W))
+    val tl_i = new TL_H2D()
     val bundle = Decoupled(new ModuleABundle) // here we create a Decoupled interface.
   })
 
